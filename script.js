e305312198c0f2fb6c463f49b4d0060b0cc9329d
mwatson-27 <mwatson27@student.sacs.nsw.edu.au>
2025-11-14 00:23:31 +0000

    Add comment to script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //john
+const grid_area = document.getElementById("grid-holder")
+const spawn_button = document.getElementById("spawn-grid")
+
+function spawn_grid(width, height) {
+    for (let i = 0; i++; i < height) {
+        for (let i = 0; i++; i < width) {
+            grid_area.appendChild(`<div id="square${1}" class="grid-item off"></div>`)
+        }
+    }
+}
+
+
